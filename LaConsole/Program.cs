67f160d0dfa4455ab052e5b3e88a43b223d8fcb8
UnderTheRadarCS <36223505+UnderTheRadarCS@users.noreply.github.com>
2018-02-07 09:15:12 +0100

    Initial commit

---
+++ LaConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using MaBibli;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaConsole
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MyCircle carre = new MyCircle(10, new MyPoint(5, 10));
+			Console.WriteLine("\n\n" + carre.IsPointIn(new MyPoint(15, 20)));
+			while (1 == 1) ;
+		}
+	}
+}
